fde5b594f854250b3337bae4088e99a927c754d2
thomascl24 <thomascl@berkeley.edu>
2025-07-23 23:34:18 -0700

    modified Toaster import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navigation } from "@/components/ui/navigation";
 import Home from "@/pages/home";
