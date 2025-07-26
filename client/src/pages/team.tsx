f9ea7cc14dbd531407a4ad5fdfd4ba89bc0462cc
R Jag <rjagan@berkeley.edu>
2025-07-26 03:36:51 +0000

    small change to Victoria's desc

--- client/src/pages/team.tsx
+++ client/src/pages/team.tsx
@@ -40,8 +40,8 @@ export default function Team() {
       name: "Victoria Brendel", 
       role: "Machine Learning Engineer", 
       email: "victoriabrendel@berkeley.edu",
-      description: `Data scientist with 2 years of experience in manufacturing operations. Currently employed as an operations engineer at SpaceX
-      working on Falcon and Dragon production. Current MIDS student at UC Berkeley with a BA in Astrophysics from UC Berkeley.`,
+      description: `Data scientist with 2 years of experience in manufacturing operations. Employed as an operations engineer at SpaceX
+      supporting Falcon and Dragon production. Current MIDS student at UC Berkeley with a BA in Astrophysics from UC Berkeley.`,
       image: victoriaImg 
     }
   ];
